13326c29b9fa3003378e3211c53f012853ec80b6
Mohamed Elsayed <mohamedelsayed1996101@outlook.com>
2019-10-07 01:12:30 +0200

    update

---
+++ Microservice-Course/limits-services/src/main/java/com/microservice/limitsservices/bean/LimitConfiguration.java
@@ -0,0 +1,30 @@
+package com.microservice.limitsservices.bean;
+
+public class LimitConfiguration {
+	private int minmum;
+	private int maxmum;
+	
+	public LimitConfiguration() {
+
+		// TODO Auto-generated constructor stub
+	}
+	public LimitConfiguration(int minmum, int maxmum) {
+		super();
+		this.minmum = minmum;
+		this.maxmum = maxmum;
+	}
+	public int getMinmum() {
+		return minmum;
+	}
+	public void setMinmum(int minmum) {
+		this.minmum = minmum;
+	}
+	public int getMaxmum() {
+		return maxmum;
+	}
+	public void setMaxmum(int maxmum) {
+		this.maxmum = maxmum;
+	}
+	
+
+}
